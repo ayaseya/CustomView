37d09ef983059bab039333a8005ff969f3b4abb2
1218AND <1218AND@6A-12>
2014-02-13 14:43:35 +0900

    0.0.01(Alpha)

---
+++ src/com/example/trump/Deck.java
@@ -0,0 +1,11 @@
+package com.example.trump;
+
+import java.util.ArrayList;
+
+public class Deck {
+	ArrayList<Trump> deck;
+
+	public Deck() {
+		deck = new ArrayList<Trump>();
+	}
+}
---
+++ src/com/example/trump/Trump.java
@@ -0,0 +1,15 @@
+package com.example.trump;
+
+public class Trump {
+	
+	//トランプの数字(1～13)
+	private int number; 
+	//トランプの図柄(1234:♠♡♣♢)
+	private int suit;
+
+	public Trump(int number, int suit) {
+		this.number = number;
+		this.suit = suit;
+	}
+
+}
